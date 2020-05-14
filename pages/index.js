2408bcdbf56d5841b8fb242324a66056b713f884
Ricardo Pereira <ricardo.pereira@farfetch.com>
2020-05-14 15:55:53 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//for static folder
+// export default () => (
+//   <img src="/static/logo.svg"/>
+// )
+//for public folder
+export default () => (
+  <img src="/logo.svg"/>
+)
